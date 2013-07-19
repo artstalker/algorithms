a89ff8156f198e7b24b25a92e153ba1ce09287e7
artstalker <groshevoy@gmail.com>
2013-07-19 18:00:07 +0300

    123

---
+++ Assign-Collinear-Points/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Assign_Collinear_Points
+{
+	class Program
+	{
+
+		static  List<Point>  Points = new List<Point>();
+		static void Main(string[] args)
+		{
+			var lines = File.ReadAllLines(args[0]).Skip(1);
+
+			foreach (var line in lines)
+			{
+				var vars = line.Split(' ');
+				int x = int.Parse(vars[0]);
+				int y = int.Parse(vars[1]);
+				Points.Add(new Point(x,y));
+			}
+
+		}
+	}
+
+	class Point
+	{
+		public int X;
+
+		public int Y;
+
+		public Point(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+		public override string ToString()
+		{
+
+			return string.Format("{{0},{1}}", X, Y);
+		}
+
+		public double SlopeTo(Point p)
+		{
+			return 0;
+		}
+	}
+}
